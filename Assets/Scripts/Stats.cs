86261c047a1f79b1c945ae43c00f5d2178d8d574
maroin.kassas@gmail.com <maroin.kassas@gmail.com>
2024-06-03 02:40:35 +0200

    Stats

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public int healthPoint = 100;
+
+    public int actionPoint = 5;
+
+    public int movementPoint = 3;
+}
