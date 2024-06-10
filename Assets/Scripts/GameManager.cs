a0dbb178756620d4d0a6ed3a14bbd63deeadf83c
maroin.kassas@gmail.com <maroin.kassas@gmail.com>
2024-06-10 13:22:12 +0200

    Correction

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,23 +5,5 @@ using static UnityEngine.GraphicsBuffer;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject player;
-
-    void Start()
-    {
-    }
-
-    void Update()
-    {
-        if (Physics.Raycast(player.transform.position, -Vector3.up, out RaycastHit raycastHit, 1))
-        {
-            if (raycastHit.collider.GetComponent<Character>())
-            {
-                Character character = raycastHit.collider.GetComponent<Character>();
-                player.GetComponent<Character>().SetIntoBattle(true);
-                character.SetIntoBattle(true);
-                BattleManager.Init();
-            }
-        }
-    }
+ 
 }
